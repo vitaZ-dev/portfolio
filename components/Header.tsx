07c376b69e717dab099498e3d067bb9a593e707b
vitaZ-dev <vitaminzdev@gmail.com>
2023-09-25 16:51:29 +0900

    setting: 컴포넌트 구조 세팅

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div>header title</div>
+      <div>
+        <span>intro </span>
+        <span>about me </span>
+        <span>Tech Stack </span>
+        <span>projects </span>
+        {/* <span>경력(취업하고 추가)</span> */}
+      </div>
+    </header>
+  );
 };
 
 export default Header;

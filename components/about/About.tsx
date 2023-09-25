4154dc60ccdbf365f69c0176a3ef9170920022e2
vitaZ-dev <vitaminzdev@gmail.com>
2023-09-25 18:13:28 +0900

    feat: about 페이지 작업중

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section>
+      <h2>about me</h2>
+      <ul>
+        <li>이름</li>
+        <li>생일</li>
+        <li>주소</li>
+        <li>email</li>
+        <li>학과</li>
+        <li>git</li>
+        <li>notion</li>
+      </ul>
+    </section>
+  );
 };
 
 export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import About from '@/components/about/About';
 import Intro from '@/components/intro/Intro';
 
 export default function Home() {
   return (
     <>
       <Intro />
+      <About />
     </>
   );
 }

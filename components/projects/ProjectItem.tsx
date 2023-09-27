bfd105317aa965a24bc481413727351e506bcaea
vitaZ-dev <vitaminzdev@gmail.com>
2023-09-27 17:02:34 +0900

    feat: project 페이지 작업중

--- components/projects/ProjectItem.tsx
+++ components/projects/ProjectItem.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const ProjectItem = () => {
-  return <div>ProjectItem</div>;
+  return (
+    <div className="project-item">
+      웹 포트폴리오 solo project <br />
+      작업기간 2022.05.12 ~ 2022.05.14 (solo/협업)
+      <div style={{ display: 'flex', padding: '2.5rem' }}>
+        <div className="thumbnail"></div>
+        <div>
+          <FontAwesomeIcon icon={faCheck} /> 소개글 <br />
+          <FontAwesomeIcon icon={faCheck} /> 주요기능? 2(FE) <br />
+          <FontAwesomeIcon icon={faCheck} /> 작업인원 2(FE) <br />
+          <FontAwesomeIcon icon={faCheck} /> 사용기술 HTML5 CSS3 JavaScript <br />
+          <FontAwesomeIcon icon={faCheck} /> 작업파트 HTML 마크업 CSS 스타일링 <br />
+          <FontAwesomeIcon icon={faCheck} /> 배포 vercel? <br />
+          <FontAwesomeIcon icon={faCheck} /> 깃허브 github <br />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectItem;
--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faCheck } from '@fortawesome/free-regular-svg-icons';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import ProjectItem from './ProjectItem';
 
 const Projects = () => {
   return (
@@ -14,17 +12,7 @@ const Projects = () => {
           <li>포트폴리오 제작</li>
           <li>퍼블리싱 - swiper</li>
         </ul>
-        <div style={{ width: '75%', background: 'aliceblue', margin: '0 auto' }}>
-          project-name <br />
-          <FontAwesomeIcon icon={faCheck} /> 소개글 <br />
-          <FontAwesomeIcon icon={faCheck} /> 주요기능? 2(FE) <br />
-          <FontAwesomeIcon icon={faCheck} /> 작업인원 2(FE) <br />
-          <FontAwesomeIcon icon={faCheck} /> 작업기간 2022.05.12 ~ 2022.05.14 <br />
-          <FontAwesomeIcon icon={faCheck} /> 사용기술 HTML5 CSS3 JavaScript <br />
-          <FontAwesomeIcon icon={faCheck} /> 작업파트 HTML 마크업 CSS 스타일링 <br />
-          <FontAwesomeIcon icon={faCheck} /> 배포 vercel? <br />
-          <FontAwesomeIcon icon={faCheck} /> 깃허브 github <br />
-        </div>
+        <ProjectItem />
       </div>
     </section>
   );

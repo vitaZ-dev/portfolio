2e397a24f672e4d9152c434f15be66639ea8e636
vitaZ-dev <vitaminzdev@gmail.com>
2023-10-04 11:12:21 +0900

    test: swiper 버튼 테스트

--- components/projects/Swipertest.tsx
+++ components/projects/Swipertest.tsx
@@ -8,15 +8,50 @@ import { Navigation } from 'swiper/modules';
 const SwiperTest = () => {
   return (
     <>
-    <Swiper
-    className="item-swiper"
-    navigation={true}
-    loop={true}
-    slidesPerView={3}
-    slidesPerGroup={3}
-    spaceBetween={50}
-    modules={[Navigation]}>
-        <SwiperSlide>Slide 1</SwiperSlide>
+    <>
+      <div className="swiper-prev-btn">
+        이전버튼
+      </div>
+      <Swiper
+        id="item-swiper"
+        navigation={{
+          prevEl: '.swiper-prev-btn',
+          nextEl: '.swiper-next-btn',
+        }}
+        modules={[Navigation]}
+        loop={true}
+        slidesPerView={1}
+        slidesPerGroup={1}
+        spaceBetween={25}
+        breakpoints={{
+          826: {
+            slidesPerView: 2,
+          },
+          // 1024px 이상일 때
+          1024: {
+            slidesPerView: 2,
+          },
+        }}>
+        <SwiperSlide>
+        <div className="project-item">
+          <div className="item-title">
+            웹 포트폴리오 solo project <br />
+            작업기간 2022.05.12 ~ 2022.05.14 (solo/협업)
+          </div>
+          <div className="item-box">
+            <div className="item-thumbnail"></div>
+            <div className="item-detail">
+              소개글 <br />
+              주요기능? 2(FE) <br />
+              작업인원 2(FE) <br />
+              사용기술 HTML5 CSS3 JavaScript <br />
+              작업파트 HTML 마크업 CSS 스타일링 <br />
+              배포 vercel? <br />
+              깃허브 github <br />
+            </div>
+          </div>
+        </div>
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
@@ -26,6 +61,10 @@ const SwiperTest = () => {
         <SwiperSlide>Slide 8</SwiperSlide>
         <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
+      <div className="swiper-next-btn">
+        다음버튼
+      </div>
+    </>
     </>
   );
 };
